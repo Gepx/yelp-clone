7c8c50321a024a52ae11924b6caf26c82ec98d27
egipsinargo <egipsinargo123@gmail.com>
2024-09-28 11:02:48 +0700

    menggunakan API Unsplash untuk memanggil image

--- seeds/place.js
+++ seeds/place.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Place = require("../models/place");
+const { getImageFromUnsplash } = require("../utils/unsplash");
 // const hereMaps = require("../utils/hereMaps");
 
 mongoose
@@ -19,7 +20,7 @@ async function seedPlaces() {
       description:
         "Taman hiburan keluarga dengan berbagai replika bangunan dari seluruh Indonesia",
       location: "Taman Mini Indonesia Indah, Jakarta",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pantai Kuta",
@@ -27,7 +28,7 @@ async function seedPlaces() {
       description:
         "Pantai yang terkenal di Bali dengan pemandangan sunset yang indah",
       location: "Pantai Kuta, Kuta, Badung Regency, Bali",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Borobudur",
@@ -35,7 +36,7 @@ async function seedPlaces() {
       description:
         "Candi Buddha terbesar di dunia yang terletak di Magelang, Jawa Tengah",
       location: "Borobudur, Magelang, Central Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Kawah Putih",
@@ -43,7 +44,7 @@ async function seedPlaces() {
       description:
         "Kawah vulkanik dengan danau berwarna putih di Bandung, Jawa Barat",
       location: "Kawah Putih, Ciwidey, West Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Malioboro",
@@ -51,7 +52,7 @@ async function seedPlaces() {
       description:
         "Jalan utama di Yogyakarta dengan berbagai toko dan kuliner khas",
       location: "Jl. Malioboro, Yogyakarta City, Special Region of Yogyakarta",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pantai Tanjung Aan",
@@ -59,7 +60,7 @@ async function seedPlaces() {
       description:
         "Pantai dengan pasir berwarna putih dan air laut yang jernih di Lombok, Nusa Tenggara Barat",
       location: "Pantai Tanjung Aan, Lombok, West Nusa Tenggara",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Bukit Bintang",
@@ -67,7 +68,7 @@ async function seedPlaces() {
       description: "Kawasan perbelanjaan dan hiburan di Kuala Lumpur, Malaysia",
       location:
         "Bukit Bintang, Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Candi Prambanan",
@@ -75,7 +76,7 @@ async function seedPlaces() {
       description:
         "Candi Hindu terbesar di Indonesia yang terletak di Yogyakarta",
       location: "Candi Prambanan, Sleman, Special Region of Yogyakarta",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Danau Toba",
@@ -83,7 +84,7 @@ async function seedPlaces() {
       description:
         "Danau vulkanik terbesar di Indonesia yang terletak di Sumatera Utara",
       location: "Danau Toba, North Sumatra",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Kawah Ijen",
@@ -91,7 +92,7 @@ async function seedPlaces() {
       description:
         "Kawah vulkanik dengan fenomena blue fire di Banyuwangi, Jawa Timur",
       location: "Kawah Ijen, Banyuwangi, East Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pantai Sanur",
@@ -99,7 +100,7 @@ async function seedPlaces() {
       description:
         "Pantai di Bali yang cocok untuk berenang dan melihat matahari terbit",
       location: "Pantai Sanur, Denpasar, Bali",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
 
     {
@@ -108,7 +109,7 @@ async function seedPlaces() {
       description:
         "Candi Buddha terbesar di dunia yang terletak di Magelang, Jawa Tengah",
       location: "Candi Borobudur, Borobudur, Magelang, Central Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pulau Komodo",
@@ -116,7 +117,7 @@ async function seedPlaces() {
       description:
         "Pulau di Indonesia yang terkenal dengan komodo, hewan terbesar di dunia",
       location: "Pulau Komodo, East Nusa Tenggara",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Taman Nasional Gunung Rinjani",
@@ -124,7 +125,7 @@ async function seedPlaces() {
       description:
         "Taman nasional yang terletak di Lombok dan memiliki gunung tertinggi kedua di Indonesia",
       location: "Taman Nasional Gunung Rinjani, Lombok, West Nusa Tenggara",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Bukit Tinggi",
@@ -132,7 +133,7 @@ async function seedPlaces() {
       description:
         "Kota kecil yang terletak di Sumatera Barat dengan arsitektur khas Eropa",
       location: "Bukit Tinggi, West Sumatra",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pulau Weh",
@@ -140,7 +141,7 @@ async function seedPlaces() {
       description:
         "Pulau yang terletak di ujung barat Indonesia dengan keindahan bawah laut yang luar biasa",
       location: "Pulau Weh, Sabang, Aceh",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Taman Safari Indonesia",
@@ -148,7 +149,7 @@ async function seedPlaces() {
       description:
         "Taman hiburan keluarga dengan berbagai satwa liar di Cisarua, Bogor",
       location: "Taman Safari Indonesia, Cisarua, West Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Gunung Merbabu",
@@ -156,7 +157,7 @@ async function seedPlaces() {
       description:
         "Gunung yang terletak di Jawa Tengah dengan pemandangan matahari terbit yang indah",
       location: "Gunung Merbabu, Central Java",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Pulau Lombok",
@@ -164,7 +165,7 @@ async function seedPlaces() {
       description:
         "Pulau di Indonesia yang terkenal dengan keindahan pantainya",
       location: "Pulau Lombok, West Nusa Tenggara",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
     {
       title: "Tanjung Lesung",
@@ -172,7 +173,7 @@ async function seedPlaces() {
       description:
         "Kawasan wisata pantai di Banten yang cocok untuk bersantai dan berenang",
       location: "Tanjung Lesung, Pandeglang, Banten",
-      image: "https://source.unsplash.com/collection/2349781/1280x720",
+      image: await getImageFromUnsplash(786923),
     },
   ];
 
