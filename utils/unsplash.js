f2291999c2d68a5d35a036912fcac2d078a1b222
egipsinargo <egipsinargo123@gmail.com>
2024-09-28 11:32:46 +0700

    mengubah size pada image yang dipanggil

--- utils/unsplash.js
+++ utils/unsplash.js
@@ -3,14 +3,24 @@ const fetch = require("node-fetch");
 const accessKey = "LJ9kZ-LO-JI3VIzHm9g_sgNq3cED6j7q4NwqKxNJwAE";
 
 async function getImageFromUnsplash(collectionId) {
+  // try {
+  //   const response = await fetch(
+  //     `https://api.unsplash.com/photos/random/?client_id=${accessKey}&collections=${collectionId}&count=1`
+  //   );
+  //   const data = await response.json();
+  //   return data[0].urls.regular; // Mengembalikan URL gambar
+  // } catch (err) {
+  //   console.log("Error fetching image from Unsplash:", err);
+  // }
   try {
     const response = await fetch(
       `https://api.unsplash.com/photos/random/?client_id=${accessKey}&collections=${collectionId}&count=1`
     );
     const data = await response.json();
-    return data[0].urls.regular; // Mengembalikan URL gambar
+    const imageUrl = `${data[0].urls.raw}&w=1280&h=720&fit=crop`; // Menambahkan parameter resolusi
+    return imageUrl;
   } catch (err) {
-    console.log("Error fetching image from Unsplash:", err);
+    console.log("Error fetching image:", err);
   }
 }
 
