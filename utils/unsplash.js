4669697eaea0f65e3582e10debbb0faba85e5b6a
egipsinargo <egipsinargo123@gmail.com>
2024-09-28 11:03:04 +0700

    mengoneksikan API Unsplash

---
+++ utils/unsplash.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+const accessKey = "LJ9kZ-LO-JI3VIzHm9g_sgNq3cED6j7q4NwqKxNJwAE";
+
+async function getImageFromUnsplash(collectionId) {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random/?client_id=${accessKey}&collections=${collectionId}&count=1`
+    );
+    const data = await response.json();
+    return data[0].urls.regular; // Mengembalikan URL gambar
+  } catch (err) {
+    console.log("Error fetching image from Unsplash:", err);
+  }
+}
+
+module.exports = { getImageFromUnsplash };
