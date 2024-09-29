a1b6dc4305f434dee32115a90c3a77e25d06e1a7
egipsinargo <egipsinargo123@gmail.com>
2024-09-29 21:24:01 +0700

    menambahkan objek review

--- models/place.js
+++ models/place.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
@@ -7,6 +8,18 @@ const placeSchema = new Schema({
   description: String,
   location: String,
   image: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+placeSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await review.deleteMany({ _id: { $in: doc.reviews } });
+  }
 });
 
 module.exports = mongoose.model("Place", placeSchema);
