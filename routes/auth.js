3281bc2a4a7161a0854fc62aabee9ce140711b3a
egipsinargo <egipsinargo123@gmail.com>
2024-09-30 23:08:08 +0700

    membuat registrasi user dan password (authentication)

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 // app.get("/register", async (req, res) => {
 //     const user = new User({
@@ -27,14 +28,46 @@ router.post(
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
-      await User.register(user, password);
-      req.flash("success_msg", "You are registered and can log in");
-      res.redirect("/places");
+      const registerUser = await User.register(user, password);
+      req.login(registerUser, (err) => {
+        if (err) return next(err);
+        req.flash("success_msg", "You are registered and can log in");
+        res.redirect("/places");
+      });
     } catch (error) {
       req.flash("error_msg", error.message);
       res.redirect("/register");
     }
   })
 );
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: {
+      type: "error_msg",
+      msg: "Invalid username or password",
+    },
+  }),
+  (req, res) => {
+    req.flash("success_msg", " You are logged in");
+    res.redirect("/places");
+  }
+);
+
+router.post("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success_msg", "You are logged out");
+    res.redirect("/login");
+  });
+});
 
 module.exports = router;
