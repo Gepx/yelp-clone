fb20547ff1e5256a80c1a584bb60f64d2b3a7734
egipsinargo <egipsinargo123@gmail.com>
2024-09-30 23:07:04 +0700

    mengoneksikan dan menggunakan middleware

--- routes/places.js
+++ routes/places.js
@@ -3,6 +3,7 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
 const isValidObjectId = require("../middlewares/isValidObjectId");
+const isAuth = require("../middlewares/isAuth");
 
 // models
 const Place = require("../models/place");
@@ -28,12 +29,13 @@ router.get(
   })
 );
 
-router.get("/create", (req, res) => {
+router.get("/create", isAuth, (req, res) => {
   res.render("places/create");
 });
 
 router.post(
   "/",
+  isAuth,
   validatePlace,
   wrapAsync(async (req, res, next) => {
     const place = new Place(req.body.place);
@@ -54,6 +56,7 @@ router.get(
 
 router.get(
   "/:id/edit",
+  isAuth,
   isValidObjectId("/places"),
   wrapAsync(async (req, res) => {
     const place = await Place.findById(req.params.id);
@@ -63,6 +66,7 @@ router.get(
 
 router.put(
   "/:id",
+  isAuth,
   validatePlace,
   isValidObjectId("/places"),
   wrapAsync(async (req, res) => {
@@ -76,6 +80,7 @@ router.put(
 
 router.delete(
   "/:id",
+  isAuth,
   isValidObjectId("/places"),
   wrapAsync(async (req, res) => {
     await Place.findByIdAndDelete(req.params.id);
