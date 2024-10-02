f4c31455068e0eb1f645178f79dceab8586eac56
egipsinargo <egipsinargo123@gmail.com>
2024-10-02 22:31:45 +0700

    mengoneksikan author review

--- routes/reviews.js
+++ routes/reviews.js
@@ -11,6 +11,7 @@ const Review = require("../models/review");
 
 // schemas
 const { reviewSchema } = require("../schemas/review");
+const { isAuthorReview } = require("../middlewares/isAuthor");
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
@@ -28,19 +29,25 @@ router.post(
   validateReview,
   isValidObjectId("/places"),
   wrapAsync(async (req, res) => {
+    const { place_id } = req.params;
+
     const review = new Review(req.body.review);
-    const place = await Place.findById(req.params.place_id);
-    place.reviews.push(review);
+    review.author = req.user._id;
     await review.save();
+
+    const place = await Place.findById(place_id);
+    place.reviews.push(review);
     await place.save();
+
     req.flash("success_msg", "Review added successfully!");
-    res.redirect(`/places/${req.params.place_id}`);
+    res.redirect(`/places/${place_id}`);
   })
 );
 
 router.delete(
   "/:review_id",
   isAuth,
+  isAuthorReview,
   isValidObjectId("/places"),
   wrapAsync(async (req, res) => {
     const { place_id, review_id } = req.params;
