ed603a07aa778e4bb24353518afbeb7e32ee7a07
egipsinargo <egipsinargo123@gmail.com>
2024-10-03 11:11:41 +0700

    menyederhanakan route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 
 // controller
 const AuthController = require("../controllers/auth");
+const { route } = require("./places");
 
 // app.get("/register", async (req, res) => {
 //     const user = new User({
@@ -21,23 +22,24 @@ const AuthController = require("../controllers/auth");
 //     }
 //   });
 
-router.get("/register", AuthController.registerForm);
+router
+  .route("/register")
+  .get(AuthController.registerForm)
+  .post(wrapAsync(AuthController.register));
 
-router.post("/register", wrapAsync(AuthController.register));
-
-router.get("/login", AuthController.loginForm);
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: {
-      type: "error_msg",
-      msg: "Invalid username or password",
-    },
-  }),
-  AuthController.login
-);
+router
+  .route("/login")
+  .get(AuthController.loginForm)
+  .post(
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: {
+        type: "error_msg",
+        msg: "Invalid username or password",
+      },
+    }),
+    AuthController.login
+  );
 
 router.post("/logout", AuthController.logout);
 
