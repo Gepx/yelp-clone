6ea8717134b625ea355e711e6a3a7952c83afae4
egipsinargo <egipsinargo123@gmail.com>
2024-10-03 15:15:42 +0700

    mengubah fungsi update dan destroy

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,6 @@
 const Place = require("../models/place");
+const fs = require("fs");
+const ExpressError = require("../utils/ErrorHandler");
 
 module.exports.index = async (req, res) => {
   const places = await Place.find();
@@ -6,9 +8,16 @@ module.exports.index = async (req, res) => {
 };
 
 module.exports.store = async (req, res, next) => {
+  const images = req.files.map((file) => ({
+    url: file.path,
+    filename: file.filename,
+  }));
   const place = new Place(req.body.place);
   place.author = req.user._id;
+  place.images = images;
+
   await place.save();
+
   req.flash("success_msg", "Place added successfully!");
   res.redirect("/places");
 };
@@ -31,15 +40,40 @@ module.exports.edit = async (req, res) => {
 };
 
 module.exports.update = async (req, res) => {
-  await Place.findByIdAndUpdate(req.params.id, {
+  const place = await Place.findByIdAndUpdate(req.params.id, {
     ...req.body.place,
   });
+
+  if (req.files && req.files.length > 0) {
+    place.images.forEach((image) => {
+      fs.unlink(image.url, (err) => new ExpressError(err));
+    });
+
+    const images = req.files.map((file) => ({
+      url: file.path,
+      filename: file.filename,
+    }));
+    place.images = images;
+    await place.save();
+  }
+
   req.flash("success_msg", "Place updated successfully!");
   res.redirect(`/places/${req.params.id}`);
 };
 
 module.exports.destroy = async (req, res) => {
-  await Place.findByIdAndDelete(req.params.id);
+  // await Place.findByIdAndDelete(req.params.id);
+  const { id } = req.params;
+  const place = await Place.findById(id);
+
+  if (place.images.length > 0) {
+    place.images.forEach((image) => {
+      fs.unlink(image.url, (err) => new ExpressError(err));
+    });
+  }
+
+  await place.deleteOne();
+
   req.flash("success_msg", "Place deleted successfully!");
   res.redirect("/places");
 };
