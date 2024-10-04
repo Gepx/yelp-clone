d40c3e8ddbf055ec25e95e5ae84a7f75a4307d07
egipsinargo <egipsinargo123@gmail.com>
2024-10-04 23:05:05 +0700

    memperbaiki sedikit kesalahan pada api unsplash

--- utils/unsplash.js
+++ utils/unsplash.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-const accessKey = "LJ9kZ-LO-JI3VIzHm9g_sgNq3cED6j7q4NwqKxNJwAE";
+const accessKey = "6bAsvB7306PHJmUfoaCGOipSegB_5NKzCql_LISRJcc";
 
 async function getImageFromUnsplash(collectionId) {
   // try {
@@ -14,13 +14,16 @@ async function getImageFromUnsplash(collectionId) {
   // }
   try {
     const response = await fetch(
-      `https://api.unsplash.com/photos/random/?client_id=${accessKey}&collections=${collectionId}&count=1`
+      `https://api.unsplash.com/photos/random/?client_id=${accessKey}&collections=${collectionId}&count=1`,
+      {
+        timeout: 10000,
+      }
     );
     const data = await response.json();
     const imageUrl = `${data[0].urls.raw}&w=1280&h=720&fit=crop`; // Menambahkan parameter resolusi
     return imageUrl;
   } catch (err) {
-    console.log("Error fetching image:", err);
+    console.log("Error fetching image:", err.message);
   }
 }
 
